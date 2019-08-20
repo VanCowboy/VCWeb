2209f77f4dc1c62fb71a3a0cd764f445e399744d
vancowboy <github@vancowboy.com>
2019-08-20 14:39:50 -0700

    Dogs mostly done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -23,7 +24,7 @@ import 'hammerjs';
     VacationComponent,
     PageNotFoundComponent,
     FooterComponent,
-    Nola2019Component
+    Nola2019Component,
   ],
   imports: [
     BrowserModule,
--- src/app/dogs/dogs.component.ts
+++ src/app/dogs/dogs.component.ts
@@ -7,36 +7,58 @@ import { Component } from '@angular/core';
 })
 export class DogsComponent {
 
+
   images = [
     {
       text: 'Allie Girl',
-      image: '../../assets/images/dogs/allie.jpg'
+      image: '../../assets/images/dogs/allie.jpg',
+      anchor: 'Allie'
     },
     {
       text: 'Brocolli',
-      image: '../../assets/images/dogs/Broc1.jpg'
+      image: '../../assets/images/dogs/Broc1.jpg',
+      anchor: 'Brocolli'
     },
     {
       text: 'Charlie',
-      image: '../../assets/images/dogs/charlie.jpg'
+      image: '../../assets/images/dogs/charlie.jpg',
+      anchor: 'Charlie'
     },
     {
       text: 'Guinness',
-      image: '../../assets/images/dogs/guiness.jpg'
+      image: '../../assets/images/dogs/guiness.jpg',
+      anchor: 'Guinness'
     },
     {
       text: 'Pumpkin',
-      image: '../../assets/images/dogs/pumpkin.jpg'
+      image: '../../assets/images/dogs/pumpkin.jpg',
+      anchor: 'Pumpkin'
     },
     {
       text: 'Shylo',
-      image: '../../assets/images/dogs/shylo.jpg'
+      image: '../../assets/images/dogs/shylo.jpg',
+      anchor: 'Shylo'
     },
     {
       text: 'Zues',
-      image: '../../assets/images/dogs/Broc1.jpg'
+      image: '../../assets/images/dogs/Broc1.jpg',
+      anchor: 'Zues'
     }
   ];
+
+  ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (event: any): void => {
+    const number = event.srcElement.scrollTop;
+    console.log(event);
+    console.log('I am scrolling ' + number);
+  }
 
   constructor() {}
 
